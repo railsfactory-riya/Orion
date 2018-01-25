ecba8149b63da21ea180dc7ee795585fa40e8751
railsfactory-riya <riya@railsfactory.org>
2018-01-25 16:17:31 +0530

    Loadging Page add

--- src/components/EmployeeInfo/EmployeeDetails.js
+++ src/components/EmployeeInfo/EmployeeDetails.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
+import PageLoading from '../PageLoading';
 import { allEmp } from '../../actions/User';
 
 class EmployeeDetails extends Component {
@@ -15,105 +16,111 @@ class EmployeeDetails extends Component {
    }
 
   render() {
-    let all_emp = (this.props.userDetailsReducer && this.props.userDetailsReducer.all_emp) ? this.props.userDetailsReducer.all_emp.users : [];
-    console.log(all_emp);
+    let all_emp = (this.props.userDetailsReducer && this.props.userDetailsReducer.all_emp) ?
+    this.props.userDetailsReducer.all_emp.users : [];
     return (
-      <div className="box">
-        <div className="box-header with-border">
-          <h3 className="box-title">All Employee Details</h3>
-        </div>
-        <div className="box-body">
-          <div className="row">
-            <div className="col-md-6">
-              <div className="input-group margin">
-                <div className="input-group-btn">
-                  <button type="button" className="btn btn-warning dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-                    Per Page&nbsp;
-                    <span className="fa fa-caret-down"></span>
-                  </button>
-                  <ul className="dropdown-menu">
-                    <li>&emsp;Show per Page</li>
-                    <li className="divider"></li>
-                    <li><a href="#">10</a></li>
-                    <li><a href="#">20</a></li>
-                    <li><a href="#">30</a></li>
-                    <li><a href="#">40</a></li>
-                    <li><a href="#">50</a></li>
-                    <li><a href="#">60</a></li>
-                    <li><a href="#">70</a></li>
-                    <li><a href="#">80</a></li>
-                    <li><a href="#">90</a></li>
-                    <li><a href="#">100</a></li>
-                  </ul>
+      <div>
+        { all_emp.length <= 0 ?
+          <PageLoading />
+          :
+          <div className="box">
+            <div className="box-header with-border">
+              <h3 className="box-title">All Employee Details</h3>
+            </div>
+            <div className="box-body">
+              <div className="row">
+                <div className="col-md-6">
+                  <div className="input-group margin">
+                    <div className="input-group-btn">
+                      <button type="button" className="btn btn-warning dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+                        Per Page&nbsp;
+                        <span className="fa fa-caret-down"></span>
+                      </button>
+                      <ul className="dropdown-menu">
+                        <li>&emsp;Show per Page</li>
+                        <li className="divider"></li>
+                        <li><a href="#">10</a></li>
+                        <li><a href="#">20</a></li>
+                        <li><a href="#">30</a></li>
+                        <li><a href="#">40</a></li>
+                        <li><a href="#">50</a></li>
+                        <li><a href="#">60</a></li>
+                        <li><a href="#">70</a></li>
+                        <li><a href="#">80</a></li>
+                        <li><a href="#">90</a></li>
+                        <li><a href="#">100</a></li>
+                      </ul>
+                    </div>
+                    <input type="text" className="form-control" />
+                    <span className="input-group-btn">
+                      <button type="button" className="btn btn-info btn-flat">Go!</button>
+                    </span>
+                  </div>
                 </div>
-                <input type="text" className="form-control" />
-                <span className="input-group-btn">
-                  <button type="button" className="btn btn-info btn-flat">Go!</button>
-                </span>
-              </div>
-            </div>
-            <table className="table table-bordered">
-              <tbody>
-                <tr>
-                  <th>Emp ID</th>
-                  <th>First Name</th>
-                  <th>Last Name</th>
-                  <th>Email</th>
-                  <th>Department</th>
-                </tr>
-                {all_emp.map((values,key) => {
-                  return(
-                    <tr key={key}>
-                      <td>{values.emp_code}</td>
-                      <td>
-                        <Link to={{
-                          pathname: '/SingleEmployee',
-                          state: {ID: values.id}
-                        }} >
-                          {values.firstname}
-                        </Link>
-                      </td>
-                      <td>{values.lastname}</td>
-                      <td>{values.email}</td>
-                      <td>{values.department_name}</td>
+                <table className="table table-bordered">
+                  <tbody>
+                    <tr>
+                      <th>Emp ID</th>
+                      <th>First Name</th>
+                      <th>Last Name</th>
+                      <th>Email</th>
+                      <th>Department</th>
                     </tr>
-                  )
-                })}
-              </tbody>
-            </table>
-          </div>
-        </div>
-        <div className="box-footer clearfix">
-          <div className="row">
-            <div className="col-sm-5">
-              <div>Showing 1 to 10 of 557 entries</div>
+                    {all_emp.map((values,key) => {
+                      return(
+                        <tr key={key}>
+                          <td>{values.emp_code}</td>
+                          <td>
+                            <Link to={{
+                              pathname: '/SingleEmployee',
+                              state: {ID: values.id}
+                            }} >
+                              {values.firstname}
+                            </Link>
+                          </td>
+                          <td>{values.lastname}</td>
+                          <td>{values.email}</td>
+                          <td>{values.department_name}</td>
+                        </tr>
+                      )
+                    })}
+                  </tbody>
+                </table>
+              </div>
             </div>
-            <div className="col-sm-7">
-              <div>
-                <ul className="pagination no-margin pull-right">
-                  <li className="paginate_button previous disabled">
-                    <a href="#">Previous</a>
-                  </li>
-                  <li className="paginate_button active">
-                    <a href="#">1</a>
-                  </li>
-                  <li className="paginate_button ">
-                    <a href="#">2</a>
-                  </li>
-                  <li className="paginate_button ">
-                    <a href="#">3</a>
-                  </li>
-                  <li className="paginate_button ">
-                    <a href="#">4</a>
-                  </li>
-                  <li className="paginate_button next" id="example1_next">
-                    <a href="#" tabIndex="0">Next</a>
-                  </li>
-                </ul>
+            <div className="box-footer clearfix">
+              <div className="row">
+                <div className="col-sm-5">
+                  <div>Showing 1 to 10 of 557 entries</div>
+                </div>
+                <div className="col-sm-7">
+                  <div>
+                    <ul className="pagination no-margin pull-right">
+                      <li className="paginate_button previous disabled">
+                        <a href="#">Previous</a>
+                      </li>
+                      <li className="paginate_button active">
+                        <a href="#">1</a>
+                      </li>
+                      <li className="paginate_button ">
+                        <a href="#">2</a>
+                      </li>
+                      <li className="paginate_button ">
+                        <a href="#">3</a>
+                      </li>
+                      <li className="paginate_button ">
+                        <a href="#">4</a>
+                      </li>
+                      <li className="paginate_button next" id="example1_next">
+                        <a href="#" tabIndex="0">Next</a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        }
       </div>
     );
   }
--- src/components/EmployeeInfo/SingleEmployee.js
+++ src/components/EmployeeInfo/SingleEmployee.js
@@ -2,6 +2,7 @@ import React,{Component} from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
+import PageLoading from '../PageLoading';
 import { singleEmpDetails } from '../../actions/User';
 
 
@@ -16,78 +17,85 @@ class SingleEmployee extends Component {
    }
 
   render() {
-  let signle_emp_details = (this.props.userDetailsReducer && this.props.userDetailsReducer.signle_emp_details) ? this.props.userDetailsReducer.signle_emp_details.user : [];
-  console.log(signle_emp_details);
+    let signle_emp_details =
+    (this.props.userDetailsReducer && this.props.userDetailsReducer.signle_emp_details) ?
+    this.props.userDetailsReducer.signle_emp_details.user : [];
     return (
       <div>
-        <div className="box box-widget widget-user">
-          <div className="widget-user-header bg-green">
-            <h3 className="widget-user-username"><b>{signle_emp_details.firstname} {signle_emp_details.lastname}</b></h3>
-            <h5 className="widget-user-desc">{signle_emp_details.designation_name}</h5>
-          </div>
-          <div className="widget-user-image">
-            <img className="img-circle" src="../dist/img/riya.jpg" alt="User Avatar" />
-          </div>
-          <div className="box-footer">
-            <div className="row">
-              <div className="col-sm-4 border-right">
-                <div className="description-block">
-                  <h5 className="description-header">{signle_emp_details.emp_code}</h5>
-                  <span className="description-text">Employee ID</span>
-                </div>
+        { signle_emp_details.length <= 0?
+          <PageLoading />
+          :
+          <div>
+            <div className="box box-widget widget-user">
+              <div className="widget-user-header bg-green">
+                <h3 className="widget-user-username"><b>{signle_emp_details.firstname} {signle_emp_details.lastname}</b></h3>
+                <h5 className="widget-user-desc">{signle_emp_details.designation_name}</h5>
               </div>
-              <div className="col-sm-4 border-right">
-                <div className="description-block">
-                  <h5 className="description-header">{signle_emp_details.username}</h5>
-                  <span className="description-text">Username</span>
-                </div>
+              <div className="widget-user-image">
+                <img className="img-circle" src="../dist/img/riya.jpg" alt="User Avatar" />
               </div>
-              <div className="col-sm-4">
-                <div className="description-block">
-                  <h5 className="description-header">{signle_emp_details.no_of_projects}</h5>
-                  <span className="description-text">Prijects</span>
+              <div className="box-footer">
+                <div className="row">
+                  <div className="col-sm-4 border-right">
+                    <div className="description-block">
+                      <h5 className="description-header">{signle_emp_details.emp_code}</h5>
+                      <span className="description-text">Employee ID</span>
+                    </div>
+                  </div>
+                  <div className="col-sm-4 border-right">
+                    <div className="description-block">
+                      <h5 className="description-header">{signle_emp_details.username}</h5>
+                      <span className="description-text">Username</span>
+                    </div>
+                  </div>
+                  <div className="col-sm-4">
+                    <div className="description-block">
+                      <h5 className="description-header">{signle_emp_details.no_of_projects}</h5>
+                      <span className="description-text">Prijects</span>
+                    </div>
+                  </div>
+                </div>
+                <hr />
+                <div className="box box-success">
+                  <div className="box-header with-border">
+                    <h3 className="box-title">About {signle_emp_details.firstname}</h3>
+                  </div>
+                  <div className="box-body">
+                    <strong>Employee code</strong>
+                    <p className="text-muted">{signle_emp_details.emp_code}</p>
+                    <hr />
+                    <strong>Username</strong>
+                    <p className="text-muted">{signle_emp_details.username}</p>
+                    <hr />
+                    <strong>Firstname</strong>
+                    <p className="text-muted">{signle_emp_details.firstname}</p>
+                    <hr />
+                    <strong>Lastname</strong>
+                    <p className="text-muted">{signle_emp_details.lastname}</p>
+                    <hr />
+                    <strong>Education</strong>
+                    <p className="text-muted">{signle_emp_details.education}</p>
+                    <hr />
+                    <strong>Gender</strong>
+                    <p className="text-muted">{signle_emp_details.gender}</p>
+                    <hr />
+                    <strong>Marital Status</strong>
+                    <p className="text-muted">{signle_emp_details.marital_status}</p>
+                    <hr />
+                    <strong>Date of Birth</strong>
+                    <p className="text-muted">{signle_emp_details.date_of_birth}</p>
+                    <hr />
+                    <strong>Blood Group</strong>
+                    <p className="text-muted">{signle_emp_details.blood_group}</p>
+                    <hr />
+                    <strong>Total Experience</strong>
+                    <p className="text-muted">{signle_emp_details.total_experience}</p>
+                  </div>
                 </div>
               </div>
             </div>
-            <hr />
-            <div className="box box-success">
-              <div className="box-header with-border">
-                <h3 className="box-title">About {signle_emp_details.firstname}</h3>
-              </div>
-              <div className="box-body">
-                <strong>Employee code</strong>
-                <p className="text-muted">{signle_emp_details.emp_code}</p>
-                <hr />
-                <strong>Username</strong>
-                <p className="text-muted">{signle_emp_details.username}</p>
-                <hr />
-                <strong>Firstname</strong>
-                <p className="text-muted">{signle_emp_details.firstname}</p>
-                <hr />
-                <strong>Lastname</strong>
-                <p className="text-muted">{signle_emp_details.lastname}</p>
-                <hr />
-                <strong>Education</strong>
-                <p className="text-muted">{signle_emp_details.education}</p>
-                <hr />
-                <strong>Gender</strong>
-                <p className="text-muted">{signle_emp_details.gender}</p>
-                <hr />
-                <strong>Marital Status</strong>
-                <p className="text-muted">{signle_emp_details.marital_status}</p>
-                <hr />
-                <strong>Date of Birth</strong>
-                <p className="text-muted">{signle_emp_details.date_of_birth}</p>
-                <hr />
-                <strong>Blood Group</strong>
-                <p className="text-muted">{signle_emp_details.blood_group}</p>
-                <hr />
-                <strong>Total Experience</strong>
-                <p className="text-muted">{signle_emp_details.total_experience}</p>
-              </div>
-            </div>
           </div>
-        </div>
+        }
       </div>
     );
   }
